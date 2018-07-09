7916453ed9cd7ff132e5f69683595533b1cc292b
RaisedDjed <pierre-alexandre.blanc@ulb.ac.be>
2018-07-09 11:35:42 +0200

    get for password 1

--- database.js
+++ database.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-app.get('/listUsers', function (req, res) {
+app.get('/listHouses', function (req, res) {
     fs.readFile( __dirname + "/" + "houses.json", 'utf8', function (err, data) {
         console.log( data );
         res.end( data );
     });
+})
+
+app.get('/isCorrectPassword', function(req, res){
+    console.log(req)
 })
 
 var server = app.listen(8080, function () {
